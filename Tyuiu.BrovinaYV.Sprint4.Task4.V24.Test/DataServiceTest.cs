eef38866c394cb39289274a6640a97880c324257
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-11-07 14:25:43 +0500

    Task4

---
+++ Tyuiu.BrovinaYV.Sprint4.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BrovinaYV.Sprint4.Task4.V24.Lib;
+
+namespace Tyuiu.BrovinaYV.Sprint4.Task4.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5]
+            {
+                {7, 9, 7, 6, 7 },
+                {9, 9, 8, 6, 7 },
+                {8, 6, 5, 6, 7 },
+                {9, 9, 7, 8, 7 },
+                {5, 9, 9, 8, 9 }
+            };
+
+            int[,] res = new int[5, 5]
+            {
+                {7, 9, 7, 1, 7 },
+                {9, 9, 1, 1, 7 },
+                {1, 1, 5, 1, 7 },
+                {9, 9, 7, 1, 7 },
+                {5, 9, 9, 1, 9 }
+            };
+            CollectionAssert.AreEqual(ds.Calculate(array), res);
+        }
+    }
+}
---
+++ Tyuiu.BrovinaYV.Sprint4.Task4.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrovinaYV.Sprint4.Task4.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrovinaYV.Sprint4.Task4.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35afe32c-ddfa-469b-aa38-58b731df59d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
